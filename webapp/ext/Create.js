98b208fcb522789d745d3e1236d04327e087f5ac
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2023-05-30 10:51:51 +0200

    update

--- webapp/ext/Create.js
+++ webapp/ext/Create.js
@@ -1,136 +1,65 @@
-sap.ui.define([], function () {
+sap.ui.define(['sap/ui/core/Fragment'], function (Fragment) {
   'use strict';
   return {
     createFail: async function (oEvent) {
-      const payloads = [
-        {
-          OrderNo: '3',
+      // create dummy data
+      const payloads = [];
+      for (let i = 1; i <= 50; i++) {
+        let payload = {
+          OrderNo: i.toString(),
           buyer: 'test@test.de',
           currency_code: 'EUR',
-        },
-        {
-          OrderNo: '3',
-          buyer: 'test@test.de',
-          currency_code: 'EURO',
-        },
-      ];
-      try {
-        const tableObject = this.byId(
-          'createactivate::OrdersList--fe::table::Orders::LineItem-innerTable'
-        );
-        const binding = tableObject.getBinding('items');
-        const model = tableObject.getModel();
+        };
 
-        let resolveFunction;
-        let rejectFunction;
-        const promise = new Promise((resolve, reject) => {
-            resolveFunction = resolve;
-            rejectFunction = reject;
-        });
-        binding.attachCreateCompleted(oEvent => {
-            let { context, success } = oEvent.getParameters();
-            if (!success) {
-              // TODO: This will fail the creation, but not reply with the backend error message
-              console.log("failed")
-              console.log(context)
-              context.delete();
-              rejectFunction(context);
-            }
-            else {
-                resolveFunction(context);
-            }
-          }, this);
-
-        const contexts = [];
-        try {
-          for (let index = 0; index < payloads.length; index++) {
-            const element = payloads[index];
-            const context = binding.create(element, true);
-            contexts.push(context);
-          }
-          binding.getModel().submitBatch(model.getUpdateGroupId());
-        } catch (error) {
-          console.log('error while creating');
-        }
-        // will crash on error
-        await Promise.all(contexts.map(context => context.created()));
-        // if(!binding.hasPendingChanges()){
-        //     console.log("sucess");
-        //     await Promise.all(contexts.map(context => context.created()));
-        // } else {
-        //     console.log("failed");
-        //     // batch request is still running, cant reset changes
-        //     model.resetChanges(model.getUpdateGroupId());
-        // }
-        await promise;
-        console.log('done');
-      } catch (error) {
-        console.log('error while creating');
-        console.log(error);
+        payloads.push(payload);
       }
-    },
-    createSuccess: async function (oEvent) {
-      const payloads = [
-        {
-          OrderNo: '3',
-          buyer: 'test@test.de',
-          currency_code: 'EUR',
-        },
-        {
-          OrderNo: '3',
-          buyer: 'test@test.de',
-          currency_code: 'EUR',
-        },
-      ];
+      // this entry will fail
+      payloads.push({
+        OrderNo: '3',
+        buyer: 'test@test.de',
+        currency_code: 'EURO',
+      });
       try {
         const tableObject = this.byId(
           'createactivate::OrdersList--fe::table::Orders::LineItem-innerTable'
         );
+        const oView = this._view;
         const binding = tableObject.getBinding('items');
         const model = tableObject.getModel();
 
-        let resolveFunction;
-        let rejectFunction;
-        const promise = new Promise((resolve, reject) => {
-            resolveFunction = resolve;
-            rejectFunction = reject;
-        });
-        binding.attachCreateCompleted(oEvent => {
+          binding.attachCreateCompleted(oEvent => {
             let { context, success } = oEvent.getParameters();
             if (!success) {
-              // TODO: This will fail the creation, but not reply with the backend error message
-              console.log("failed")
-              console.log(context)
+              // successfully deleted context
               context.delete();
-              rejectFunction(context);
             }
-            else {
-                resolveFunction(context);
-            }
-          }, this);
+          },
+          this
+        );
 
-        const contexts = [];
-        try {
-          for (let index = 0; index < payloads.length; index++) {
-            const element = payloads[index];
-            const context = binding.create(element, true);
-            contexts.push(context);
-          }
-          binding.getModel().submitBatch(model.getUpdateGroupId());
-        } catch (error) {
-          console.log('error while creating');
+        let contexts = [];
+        for (let index = 0; index < payloads.length; index++) {
+          const element = payloads[index];
+          const context = binding.create(element, true);
+          contexts.push(context);
+          context.created().catch((error) => {
+            console.log('error while creating context.created().catch(');
+          });
         }
-        // will crash on error
-        await Promise.all(contexts.map(context => context.created()));
-        // if(!binding.hasPendingChanges()){
-        //     console.log("sucess");
-        //     await Promise.all(contexts.map(context => context.created()));
-        // } else {
-        //     console.log("failed");
-        //     // batch request is still running, cant reset changes
-        //     model.resetChanges(model.getUpdateGroupId());
-        // }
-        await promise;
+        await binding.getModel().submitBatch(model.getUpdateGroupId());
+        if (!binding.hasPendingChanges()) {
+          console.log('sucess');
+        } else {
+          console.log('hasPendingChanges() true');
+          // batch request is still running, cant reset changes
+          contexts.forEach(async (context) => {
+            await context.delete(model.getUpdateGroupId());
+          });
+          // cant use resetChanges because just want to delete contexts create above
+          //model.resetChanges(model.getUpdateGroupId());
+          // show message model
+          // delete all messages after close
+        }
         console.log('done');
       } catch (error) {
         console.log('error while creating');
